24e2cdc54c046cd73cbf6f8b0d895f74fc1d9ecd
Dhruv Sharma <>
2023-10-08 22:53:03 -0600

    Add visualization for graph using plotly.

--- src/environments/GraphEnvironment.py
+++ src/environments/GraphEnvironment.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel, validator
 
 from agents.SimpleAgent import SimpleAgent
 from agents.base_agent import BaseAgent
+import plotly.graph_objects as go
 
 fake = Faker()
 
@@ -108,3 +109,51 @@ class GraphEnvironment:
         ][0]
         neighbor_nodes = list(self.graph[agent_node])
         return [self.graph.nodes[node]["agent"] for node in neighbor_nodes]
+
+    def visualize_graph_plotly(self):
+        pos = nx.spring_layout(self.graph)
+
+        edge_x = []
+        edge_y = []
+        for edge in self.graph.edges():
+            x0, y0 = pos[edge[0]]
+            x1, y1 = pos[edge[1]]
+            edge_x.extend([x0, x1, None])
+            edge_y.extend([y0, y1, None])
+
+        edge_trace = go.Scatter(
+            x=edge_x,
+            y=edge_y,
+            line=dict(width=0.5, color="#888"),
+            hoverinfo="none",
+            mode="lines",
+        )
+
+        node_x = []
+        node_y = []
+        for node in self.graph.nodes():
+            x, y = pos[node]
+            node_x.append(x)
+            node_y.append(y)
+
+        node_trace = go.Scatter(
+            x=node_x,
+            y=node_y,
+            mode="markers+text",
+            hoverinfo="text",
+            marker=dict(size=10, color="skyblue"),
+            text=[data["agent"].name for _, data in self.graph.nodes(data=True)],
+            textposition="top center",
+        )
+
+        fig = go.Figure(
+            data=[edge_trace, node_trace],
+            layout=go.Layout(
+                showlegend=False,
+                hovermode="closest",
+                margin=dict(b=0, l=0, r=0, t=0),
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            ),
+        )
+        fig.show()
